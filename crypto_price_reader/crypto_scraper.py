8a683f1f57a676fc03c39538d6929262a8c65f41
אמיר אייזין <amiraizin20@gmail.com>
2022-02-08 20:42:28 +0200

    added prettyTable to display the crypto curreny prettier

--- crypto_price_reader/crypto_scraper.py
+++ crypto_price_reader/crypto_scraper.py
@@ -1,13 +1,17 @@
 import requests
+import os
+from prettytable import PrettyTable
 
-api_key = "pk_4378f1e764fe4ac6b310101cd54197d2"
+api_key = os.environ.get('api_key')
 
 
 class CryptoCurrency:
     base_url = "https://cloud.iexapis.com/stable/crypto/"
+    prices = []
 
     def __init__(self, symbol):
         self.symbol = symbol
+        self.add_prices_to_list()
 
     @property
     def complete_url(self):
@@ -17,3 +21,20 @@ class CryptoCurrency:
     def price(self):
         req = requests.get(self.complete_url).json()
         return float(req.get('price'))
+
+    def add_prices_to_list(self):
+        CryptoCurrency.prices.append([self.price, self.symbol])
+
+    @staticmethod
+    def prices_table():
+        pt = PrettyTable(["Prices", "Crypto Name"])
+        pt.add_rows(CryptoCurrency.prices)
+        return pt
+
+    @staticmethod
+    def clean_prices():
+        CryptoCurrency.prices.clear()
+
+    @staticmethod
+    def show_prices():
+        print(CryptoCurrency.prices_table())
--- crypto_price_reader/main.py
+++ crypto_price_reader/main.py
@@ -1,10 +1,14 @@
 from crypto_scraper import CryptoCurrency
-
 import time
+import os
 
 if __name__ == '__main__':
     while True:
         smybol1 = CryptoCurrency(symbol="btcusd")
-        print(smybol1.price)
-        time.sleep(1)
+        smybol2 = CryptoCurrency(symbol="ethusd")
+        smbol3 = CryptoCurrency(symbol='dogeusdt')
 
+        CryptoCurrency.show_prices()
+        time.sleep(3)
+        CryptoCurrency.clean_prices()
+        os.system("cls")
